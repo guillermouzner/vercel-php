6c0320d77f9e7db15ed197f77ea8ce948710dce5
guillermouzner <guillermouz16@gmail.com>
2024-10-10 19:58:27 -0300

    .

---
+++ api/index.php
@@ -0,0 +1,6 @@
+<?php
+$root_index_content = file_get_contents('../index.php');
+
+// If you need to process the PHP content, you would do it here
+// For now, we'll just output it
+echo $root_index_content;
